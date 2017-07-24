0fded3af351d27d12f9dba57ec402cfba68cea4d
pedro <pedroiswang2@gmail.com>
2017-07-24 14:56:39 +0800

    印出alamofire套件中get取得的資訊

--- CloudsAPP/ViewController.swift
+++ CloudsAPP/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController
 {
@@ -14,7 +15,40 @@ class ViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
-       
+        Alamofire.request("https://httpbin.org/get").responseJSON{response in
+//            print(response.request!)
+//            print(response.response!)
+//            print(response.data!)
+//            print(response.result)
+            
+            if let array = response.result.value{
+            print("準備印出資料")
+                print(array)
+            }
+
+            if let JSON = response.result.value
+            {
+                if let dictionary = JSON as? [String:Any]
+                {
+                    
+                    if let value1 = dictionary["origin"] as? String
+                    {
+                        print("解出key為origin的值")
+                        print(value1)
+                    }
+                    
+                    if let secondJSON = dictionary["headers"] as? [String:Any]
+                    {
+                        if let value2 = secondJSON["User-Agent"] as? String
+                        {
+                            print("解出key為Headers中的User-Agent值")
+                            print(value2)
+                        }
+                    }
+                }
+                
+            }
+        }
     }
 
     override func didReceiveMemoryWarning()
